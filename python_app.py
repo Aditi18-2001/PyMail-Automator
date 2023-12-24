5ea5f6d235e84074dedb1be543bd9e08becbeb67
Aditi18-2001 <aditi.s.2019.cse@ritchennai.edu.in>
2023-12-24 18:03:51 +0530

    Initial commit

--- python_app.py
+++ python_app.py
@@ -1,40 +1,33 @@
-from email.message import EmailMessage
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
 from email import encoders
+from email.message import EmailMessage
+from email.mime.base import MIMEBase
 import ssl
 import smtplib
-import urllib.parse  # Import the urllib.parse module
 
-email_sender = 'aditijaya2018@gmail.com'
-email_password = 'mpyj igom gsoc zqck'
-email_receiver = 'aditijaya2018@gmail.com'
+email_sender='aditijaya2018@gmail.com'
+email_password='qmzx oukn oehy vkbb'
 
-subject = "Creating your first python mail"
-body = """Hurry! your first python mail has been sent. You did it!"""
+email_receiver='aditijaya2018@gmail.com'
 
-# Create a multipart email message
-em = MIMEMultipart()
-em['From'] = email_sender
-em['To'] = email_receiver
-em['Subject'] = subject
+subject="Creating your first python mail"
+body=""" Hurry! your first python mail has been sent. You did it!"""
 
-# Attach the text body
-em.attach(MIMEText(body, 'plain'))
+em=EmailMessage()
+em['From']=email_sender
+em['To']=email_receiver
+em['subject']=subject
+em.set_content(body)
 
-# Adding a file attachment
-file_path = 'D:/Users/hp/Downloads/voters ID.pdf'  # Update the file path
-file_path_encoded = urllib.parse.quote(file_path)  # Encode the file path
-
-attachment = MIMEBase('application', 'octet-stream')
-attachment.set_payload(open(file_path, 'rb').read())
+# Attach a file
+file_path = 'D:/Users/hp/Downloads/voters ID.pdf'  # Replace with the actual path to your file
+attachment = MIMEBase("application", "octet-stream")
+attachment.set_payload(open(file_path, "rb").read())
 encoders.encode_base64(attachment)
-attachment.add_header('Content-Disposition', f'attachment; filename="{file_path_encoded}"')  # Use the encoded file path
-em.attach(attachment)
+attachment.add_header("Content-Disposition", f"attachment; filename={file_path}")
+em.add_attachment(attachment)
 
-context = ssl.create_default_context()
+context=ssl.create_default_context()
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
     smtp.login(email_sender, email_password)
     smtp.sendmail(email_sender, email_receiver, em.as_string())
